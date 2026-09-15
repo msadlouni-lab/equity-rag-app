fda1c17bdd6c522b9fc504a27168e0236d152098
msadlouni-lab <ms.adlouni133@gmail.com>
2026-09-15 10:13:44 -0400

    Fix ChatPromptTemplate import path for Streamlit Cloud

--- app_research.py
+++ app_research.py
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 import plotly.express as px
